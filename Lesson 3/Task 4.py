eaa5c6b759957ec1939ea399d52d8dc2450de829
Evgenii <eaturkin@yandex.ru>
2022-12-20 12:29:07 +0500

    some changes was made

--- Lesson 3/Task 4.py
+++ Lesson 3/Task 4.py
@@ -4,18 +4,18 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
 import math
-import time
 
 number = int(input('Input number transform it to binary form: '))
 binaryNumber = []
 
 while number != 0:
-        if number % 2 == 1:
-            binaryNumber.insert(0, '1')
-        else:
-            binaryNumber.insert(0, '0')
-        number = int(number / 2)
+    if number % 2 == 1:
+        binaryNumber.insert(0, '1')
+    else:
+        binaryNumber.insert(0, '0')
+    number = int(number / 2)
 
 binNum = ''.join(binaryNumber)
 
--- Lesson 3/Task 5.py
+++ Lesson 3/Task 5.py
@@ -3,11 +3,12 @@
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 2
 
-number = int(input('Input an amount of the row of Fibonachi to both directions: '))
+number = int(
+    input('Input an amount of the row of Fibonachi to both directions: '))
 fibRow = []
 
 
-def Fib(number): # Я решил объеденить два метода в один, на сколько это правильно?
+def Fib(number):  # Я решил объеденить два метода в один, на сколько это правильно?
     if number >= 0:
         if number == 0:
             return 0
@@ -26,6 +27,6 @@ for i in range(0, number+1):
     fibRow.append(Fib(i))
     fibRow.insert(0, Fib(-i))
 
-fibRow.remove(0) #повторял 0 в листе, я не придумал ничего лучшего, чем это
+fibRow.remove(0)  # повторял 0 в листе, я не придумал ничего лучшего, чем это
 print('Fibbonachi row is:')
 print(fibRow)
