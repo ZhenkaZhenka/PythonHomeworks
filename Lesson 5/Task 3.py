c24c8eff395e2733baf46cdca31b4b69bc84dcb4
Evgenii <eaturkin@yandex.ru>
2023-01-16 12:26:50 +0500

    Task 3 for lesson 5 has been finished, added comments in Task 4 for lesson 5

--- Lesson 5/Task 3.py
+++ Lesson 5/Task 3.py
@@ -11,12 +11,12 @@ def PrintPitch(list):
     print(list[6], list[7],  list[8])
     return
 
-# Установка символа в лист
+# Установка символа игроком
 def PutSign(list, point, char): 
     list[point-1] = char
     return list
 
-# Не придумал ничего лучше, да и хотелось сделать до семинара
+# Не придумал ничего лучше, да и вроде не очень много комбинаций
 # Сравнение выйгрышных комбинаций
 def CheckWin(list):  
     if list[0] == list[1] == list[2]: return True
@@ -51,11 +51,27 @@ def CheckEmptyPoint(list):
     while True:
         try:
             point = CheckNumberOfPoint(list)
-            if not str(list[point-1]).isdigit():
+            if not str(list[point-1]).isdigit(): # Если выбранное поле - цифра, то поле не занято
                 return e
             else: return point
         except Exception as e:
             print('This point has been occupied, choise another point')
+
+# Ход игрока
+def TurnPlayer(playerName, list, dict):
+    print(playerName, 'choise the point on the pitch where you want to put your sign:')
+    point = CheckEmptyPoint(list)
+    PrintPitch(PutSign(list, point, dict[playerName]))
+    return
+    
+# Маркер на начало/повтор или конец игры
+def WannaPlay():
+    play = input()
+    if play == 'Yes'.lower() or play == 'Yep'.lower() or play == 'Yeah'.lower():
+        return True
+    else:
+        print('Ok, next time!')
+        return False
 
 # Сама игра
 def Game(list):
@@ -66,45 +82,33 @@ def Game(list):
     dict = ChoiseOfSigns(player1name, player2name, playerSign) # Использую словарь для определения символа игрока, удобно
     counter = len(list) # Максимальное количество ходов, определяемое размером поля
     while True:
-        print(player1name, 'choise the point on the pitch where you want to put your sign:')
-        point = CheckEmptyPoint(list)
-        PrintPitch(PutSign(list, point, dict[player1name]))
-        if CheckWin(list):
+        TurnPlayer(player1name, list, dict) # Ход игрока
+        if CheckWin(list): # ПРоверка победы после каждого хода игрока
             print(player1name, 'you are won, congradulation!')
             return
         counter -= 1
-        if counter == 0: break
-        print(player2name, 'choise the point on the pitch where you want to put your sign:')
-        point = CheckEmptyPoint(list)
-        PrintPitch(PutSign(list, point, dict[player2name]))
-        if CheckWin(list):
+        if counter == 0: break # Если coounter == 0, то ходов не осталось, цикл останавливается
+        TurnPlayer(player2name, list, dict) # Ход игрока
+        if CheckWin(list):# ПРоверка победы после каждого хода игрока
             print(player2name, 'you are won, congradulation!')
             return
-        counter -= 1
-        if counter == 0: break
-    if not CheckWin(list):
+        counter -= 1 # Т.к поле не четное, последний возможный ход остается за игроком 1
+    if not CheckWin(list): # Проверка, есть ли победитель. В лююбом случает, если выход из цикла произошел из-за окончания возможных ходов-победителя нет
         print('Nobody won, game has to have winner!')
     return
     
-def WannaPlay():
-    play = input()
-    if play == 'Yes'.lower() or play == 'Yep'.lower() or play == 'Yeah'.lower():
-        return True
-    else:
-        print('Ok, next time!')
-        return False
 
-def ActivationOfGame(list):
+def ActivationOfGame():
     print('Hello everyone, do you want to play? Fill in "Yes" or "No": ')
-    stateOfGame = WannaPlay()
-    while stateOfGame:
-        list = [i for i in range(1,10)]
+    stateOfGame = WannaPlay() # True- хочешь играть, False-не хочешь играть
+    while stateOfGame: # Пока stateOfGame == True, игра будет начинаться сначала
+        list = [i for i in range(1,10)] # Создание поля игры
         Game(list)
         print('Do you wanna play again?')
-        stateOfGame = WannaPlay()
+        stateOfGame = WannaPlay() 
     return
 
-ActivationOfGame(list)
+ActivationOfGame()
 
     
 
--- Lesson 5/Task 4 v 2.py
+++ Lesson 5/Task 4 v 2.py
@@ -11,6 +11,8 @@
 # Выходные данные:
 # 6A1F2D7C1A17E
 
+# Отличине от Task 4 v 1.py только в методе сжатие, здесь я использую словарь
+# Подробное описание в Task 4 v 1.py
 
 text = input('Input text to compress: ')
 compressedText = ''
@@ -18,14 +20,14 @@ compressedText = ''
 dict = {}
 def CompressText(text):
     compressedText = ''
-    for i in range(len(text)):
-        dict[text[i]] = 0
-    for i in range(len(text)):
-        if text[i] in dict.keys():
+    for i in range(len(text)): # Проходим по каждому символу в тексте и создаем уникальные ключи для словаря
+        dict[text[i]] = 0 # Каждый ключ делает целочисленным и равных 0
+    for i in range(len(text)): # Снова проходим текст для подсчета количества каждого элемента
+        if text[i] in dict.keys(): # Если символ из текста-ключ, то значение в словаре для этого ключа +1
             dict[text[i]] += 1
-    for key in dict:
+    for key in dict: # Создаем текст из пар значение+ключ
         compressedText += str(dict[key]) + key
-    dict.clear()
+    dict.clear() # чистим словарь, чтобы не создавать несколько
     return compressedText
 
 def DecompressText(text):
@@ -40,6 +42,8 @@ def DecompressText(text):
     dict.clear()
     return decompressedText
 
+
+# Тут я два раза сжимаю и раскрываю введеный текст
 compressedText = CompressText(text)
 decompressedText = DecompressText(compressedText)
 compressedText2 = CompressText(decompressedText)
